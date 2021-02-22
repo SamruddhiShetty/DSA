6425fe0bc6f808891002a041aec7be9c418cc911
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2021-02-22 13:17:20 +0530

    Update and rename Rope_Cutting_Problem to Rope_Cutting_Problem.py

--- recursion/Rope_Cutting_Problem.py
+++ recursion/Rope_Cutting_Problem.py
@@ -1,4 +1,5 @@
 #given a rope of length "length" and we have to get the maximum pieces after croping the rope according to the three given fixed lengths
+#if its possible we have to return the possible max number of pieces else return it is impossible by returning "-1"
 
 def cutRope(length, l1, l2, l3):
     if length==0:
@@ -11,6 +12,7 @@ def cutRope(length, l1, l2, l3):
         m3=cutRope(length-l3, l1, l2, l3)
         value=max(m1, m2, m3)
         
+        #this is required to get the correct value else this function will always return a valid value.
         if value==-1:
             return -1
         else:
