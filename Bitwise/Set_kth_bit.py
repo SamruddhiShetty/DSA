064d31153d13a52e7d35af124199bf13c1c40c48
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2021-03-26 13:41:33 +0530

    Create Set_kth_bit.py

---
+++ Bitwise/Set_kth_bit.py
@@ -0,0 +1,6 @@
+def SetKBit(N, K):
+  #here we are supposed to set the kth index value from right to '1'
+  
+  return (1<<K)|N
+
+#here we left shift the number 1 by k times and OR it with the number given.
