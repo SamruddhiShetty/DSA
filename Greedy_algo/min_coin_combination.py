7997a19b85b92a4b7f279ab94cc4e5f267091d29
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2021-05-02 21:34:59 +0530

    Update min_coin_combination.py

--- Greedy_algo/min_coin_combination.py
+++ Greedy_algo/min_coin_combination.py
@@ -1,3 +1,8 @@
+#Given a value V, if we want to make a change for V Rs, and we have an infinite supply of each of the denominations in Indian currency, i.e., 
+#we have an infinite supply of { 1, 2, 5, 10, 20, 50, 100, 500, 1000} valued coins/notes, what is the minimum number of coins and/or notes needed to make the change
+
+# Note that this wont work to all the test cases the best solutions is dp solution
+
 def getCoin(coins, Sum):
     coins=sorted(coins, reverse=True)
     c=res=0
