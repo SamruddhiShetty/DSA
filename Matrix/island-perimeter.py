11620f247e234eb4bfb098d57de8f28bea069c0f
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2021-05-21 19:54:36 +0530

    Update island-perimeter.py

--- Matrix/island-perimeter.py
+++ Matrix/island-perimeter.py
@@ -11,9 +11,9 @@ class Solution:
                 if grid[i][j]==1:
                     perimeter+=4
                     
-                    if i>0 and grid[i-1][j]==1:
+                    if i>0 and grid[i-1][j]==1:  # Up
                         perimeter-=2
-                    if j>0 and grid[i][j-1]==1:
+                    if j>0 and grid[i][j-1]==1:  # Left 
                         perimeter-=2
         return perimeter
         
