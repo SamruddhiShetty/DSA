8f75e9da5954958cc43dc6c3332cc462ab74e183
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2021-05-22 19:27:28 +0530

    Create Valid_Number.py

---
+++ Strings/Valid_Number.py
@@ -0,0 +1,9 @@
+#refer from leetcode
+
+import re
+
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        if (re.compile('^([+-]?(((\d+\.\d*)|(\.\d+))|(\d+))([eE][+-]?\d+)?)$').match(s)==None):
+            return False
+        return True
