64aae1b2ad03b77c483d8624a4c7c5610f022d31
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2021-06-01 12:25:04 +0530

    Create unique_path.py

---
+++ dynamic_programming/unique_path.py
@@ -0,0 +1,19 @@
+#refer problem from https://leetcode.com/problems/unique-paths/submissions/
+
+#normaly when the movement is restricted to two directions and without obstacles its DP else its backtracking
+
+def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1] * n for _ in range(m)]
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                if i == 0:
+                    dp[i][j] = dp[i][j - 1]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        
+        return dp[m - 1][n - 1]
