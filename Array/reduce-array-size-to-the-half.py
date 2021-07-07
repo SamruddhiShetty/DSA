0fadf439150cb9757772b6694e6aaa04d23cc7fd
SamruddhiShetty <64214475+SamruddhiShetty@users.noreply.github.com>
2021-07-07 14:07:42 +0530

    Create reduce-array-size-to-the-half.py

---
+++ Array/reduce-array-size-to-the-half.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        cnt=Counter(arr)
+        frequencies=sorted(cnt.values())
+        
+        ans, removed, half=0, 0, len(arr)//2
+        
+        while removed<half:
+            ans+=1
+            removed+=frequencies.pop()
+            
+        return ans
+        
